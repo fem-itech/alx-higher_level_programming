d8e59961cb9c9bf35f88748aa7b41f88a1170be1
fem-itech <femielijaholubodun@gmail.com>
2023-04-04 05:06:11 -0700

    save2

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -1,16 +1,49 @@
 #!/usr/bin/python3
-Rectangle = __import__('3-rectangle').Rectangle
+""" more practice with classes!!! """
 
-my_rectangle = Rectangle(2, 4)
-print("Area: {} - Perimeter: "
-      "{}".format(my_rectangle.area(), my_rectangle.perimeter()))
 
-print(str(my_rectangle))
-print(repr(my_rectangle))
+class Rectangle:
+    """ initialize with the width and height with value checks
+    Args:
+        width: how phat dis 4polygon gon be
+        height: how tall dis box is
+    Return: nonezo
+    """
 
-print("--")
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
 
-my_rectangle.width = 10
-my_rectangle.height = 3
-print(my_rectangle)
-print(repr(my_rectangle))
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        """ calculates the area of this rekt """
+        return self.__width * self.__height
+
+    def perimeter(self):
+        """ calculates the perimeter of this rect """
+        if self.__width is 0 or self.__height is 0:
+            return 0
+        return (self.__width * 2) + (self.__height * 2)
