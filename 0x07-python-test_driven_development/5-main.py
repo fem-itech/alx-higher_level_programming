5dd27eae67ca3ff6bfc3a54efc3eecd24670e35e
fem-itech <femielijaholubodun@gmail.com>
2023-04-07 22:04:50 -0700

    savetask

---
+++ 0x07-python-test_driven_development/5-main.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+text_indentation = __import__('5-text_indentation').text_indentation
+
+text_indentation("""This is just a test indenation file for indetation. \
+Quonam modo? Utrum igitur tibi litteram videor an totas paginas commovere? \
+Non autem hoc: igitur ne illud quidem. Fortasse id optimum, sed ubi illud: \
+Plus semper voluptatis? Teneo, inquit, finem illi videri nihil dolere. \
+Transfer idem ad modestiam vel temperantiam, quae est moderatio cupiditatum \
+rationi oboediens. Si id dicis, vicimus. Inde sermone vario sex illa a Dipylo \
+stadia confecimus. Sin aliud quid voles, postea. Quae animi affectio suum \
+cuique tribuens atque , it just a test file from lrem ipsum dont bother abouot reading. \
+beatiorem! Iam ruinas videres""")
---
+++ 0x07-python-test_driven_development/README.md
@@ -0,0 +1,1 @@
+readme file for driven development pyton 
